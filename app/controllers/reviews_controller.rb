3aa173e68c6fd4732849d1578d125985eae8051b
Chloe de Mendonca <chloe.de_mendonca@edu.escp.eu>
2026-05-20 18:42:49 +1000

    REfactoring

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,4 @@
 class ReviewsController < ApplicationController
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
-
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.create(review_params)
